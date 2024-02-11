de4b8f59f17ca65f093c704fa523c16f2edb721a
Alejo Castañeda <fcastaneda@cidenet.com.co>
2024-02-10 23:45:01 -0500

    Configuring Jest unit tests

---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+import { render } from '@testing-library/react'
+import { GifItem } from '../../src/components'
+
+
+describe('Tests on <GifItem />', () => {
+  test('should', () => {
+
+    render(<GifItem title='' url='' />)
+
+  })
+})
